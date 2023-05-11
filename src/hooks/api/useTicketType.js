aada1343d6e4c6b9c2ed4f0a337e5aac362f812e
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-05-11 19:49:21 -0300

    feat: message when doesnt have subscription

--- src/hooks/api/useTicketType.js
+++ src/hooks/api/useTicketType.js
@@ -4,19 +4,19 @@ import useToken from '../useToken';
 import * as ticketApi from '../../services/ticketApi';
 
 export default function useTicketType() {
-    const token = useToken();
+  const token = useToken();
 
-    const {
-        data: ticketType,
-        loading: ticketTypeLoading,
-        error: ticketTypeError,
-        act: getTicketsTypes,
-    } = useAsync(ticketApi.getTicketType(token), false);
+  const {
+    data: ticketType,
+    loading: ticketTypeLoading,
+    error: ticketTypeError,
+    act: getTicketsTypes,
+  } = useAsync(() => ticketApi.getTicketType(token));
 
-    return {
-        ticketType,
-        ticketTypeLoading,
-        ticketTypeError,
-        getTicketsTypes
-    }
+  return {
+    ticketType,
+    ticketTypeLoading,
+    ticketTypeError,
+    getTicketsTypes
+  };
 }
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,7 +1,41 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
 import TicketAndPayment from '../../../components/Payment';
+import useEnrollment from '../../../hooks/api/useEnrollment';
 
 export default function Payment() {
+  const { enrollment } = useEnrollment();
+
   return (
-    <TicketAndPayment/>
+    <>
+      <StyledTypography variant='h4'>Ingresso e pagamento</StyledTypography>
+      
+      {enrollment ? (
+        <TicketAndPayment enrollment={enrollment} />
+      ): (
+        <SubscriptionBoxMessage>
+          <h4>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</h4>
+        </SubscriptionBoxMessage>
+      )}
+    </>
+
   );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
+const SubscriptionBoxMessage = styled.div`
+width: 420px;
+height: 46px;
+margin: 243px auto;
+text-align: center;
+  h4{
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 23px;
+    color: #8E8E8E;
+  }
+`;
