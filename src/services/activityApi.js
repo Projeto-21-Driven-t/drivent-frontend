12ebeea73df5cc2b2c9bfe87ad73c4b030af1e12
rezendematheus <matheusreisrezende.2002@gmail.com>
2023-05-28 23:12:25 -0300

    feat: handling activities post and delete

--- src/services/activityApi.js
+++ src/services/activityApi.js
@@ -9,3 +9,23 @@ export async function getActivities(token) {
 
   return response.data;
 }
+
+export async function scheduleActivity(token, body) {
+  const response = await api.post('/activities', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function deleteScheduledActivity(token, activityId) {
+  const response = await api.delete(`/activities/${activityId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response;
+}
